d8b52d0df0a0f130089a2d28f5edd579c9082bf3
Jefferson Vantuir <jeffersonbehling@gmail.com>
2025-04-03 08:51:20 -0300

    01-fundamentos: streams e middlewares

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,28 +1,30 @@
 // const http = require('http'); // CommonJS => uso de require
 import http from 'node:http'; // ESModule => uso de import
-
+import { json } from './middlewares/json.js';
 
 const users = [] // Stateful
 
-const server = http.createServer((req, res) => {
-
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
+
+    await json(req, res)
 
     if (method == 'GET' && url == '/users') {
         return res
-            .setHeader('Content-type', 'application/json')
             .end(JSON.stringify(users))
     }
     
     if (method == 'POST' && url == '/users') {
+        console.log(req.body)
+        const { name, email } = req.body
+
         users.push({
             id: 1,
-            name: 'John Doe',
-            email: 'johndoe@example.com'
+            name,
+            email
         });
 
         return res
-            .setHeader('Content-type', 'application/json')
             .writeHead(201)
             .end();
     }
