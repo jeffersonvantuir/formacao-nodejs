ce41fd00359c22fe3a3ac66ad33de49712be384d
Jefferson Vantuir <jeffersonbehling@gmail.com>
2025-04-04 09:13:09 -0300

    01-fundamentos: Banco de dados em arquivos

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,28 +1,33 @@
 // const http = require('http'); // CommonJS => uso de require
 import http from 'node:http'; // ESModule => uso de import
+import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js';
+import {Database} from "./database.js";
 
 const users = [] // Stateful
+const database = new Database('users');
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
     await json(req, res)
 
-    if (method == 'GET' && url == '/users') {
+    if (method === 'GET' && url === '/users') {
+        const users = database.select('users');
         return res
             .end(JSON.stringify(users))
     }
     
-    if (method == 'POST' && url == '/users') {
-        console.log(req.body)
+    if (method === 'POST' && url === '/users') {
         const { name, email } = req.body
 
-        users.push({
-            id: 1,
+        const user = {
+            id: randomUUID(),
             name,
             email
-        });
+        }
+
+        database.insert('users', user);
 
         return res
             .writeHead(201)
